81fea6d409106efbef80dfa7703290dc4541c2e3
Manu <manukarthik2025@gmail.com>
2026-09-24 18:00:20 +0100

    msg

--- Monopoly.py
+++ Monopoly.py
@@ -1,13 +1,24 @@
 import pygame
 import random
 pygame.init()
+
 #Screen setup
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+bg_img =  pygame.image.load("Monopoly_bg.png")
+
 running = True
+
 while running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+
             running = False
 
-bg_img = pygame.image.load("images/Monopoly_bg.png")
 screen.blit(bg_img, (0,0))
+pygame.display.flip()
+
+
+pygame.quit()
